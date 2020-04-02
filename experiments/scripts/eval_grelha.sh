32ba18346b37f7f40cdd10382f98479cb4d203b2
itv <itv@itv.org>
2020-04-02 12:04:22 -0300

    python2 modifications

--- experiments/scripts/eval_grelha.sh
+++ experiments/scripts/eval_grelha.sh
@@ -6,6 +6,6 @@ set -e
 export PYTHONUNBUFFERED="True"
 export CUDA_VISIBLE_DEVICES=0
 
-python3 ./tools/eval_linemod.py --dataset_root ./datasets/linemod/grelhas\
+python ./tools/eval_linemod.py --dataset_root ./datasets/linemod/grelhas\
   --model trained_models/linemod/pose_model_4_0.010967960115522146.pth\
   --refine_model trained_models/linemod/pose_refine_model_344_0.0037182109980759296.pth
